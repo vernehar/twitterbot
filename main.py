57f8f3286a1f568d01efaf030ba6be68cbf73a33
Verne Hartikainen <verne.hartikainen@gmail.com>
2023-01-28 14:15:14 +0200

    made a telegram bot to take sponsor message in

--- main.py
+++ main.py
@@ -13,9 +13,21 @@ def twitterapi():
     _consumer_secret = "GLltMBIBH7SN5VQJqNM03firqXe8nFb9DMcT1o0qgTv3XAzwMl"
     _access_token = "1619230600191217666-UQLY0wx0nUFlEO6RpVhGLzqmJiLA2R"
     _access_secret = "hIDythEC9CPxLD7uFzAW4VzqO3LwNvdfjS8R3DxNxTywI"
+    _client_id = "T0tPbVdJUkxkWFJoVHFBTnYxdHo6MTpjaQ"
+    _client_secret = "hahIQG7D1NPE2o9Rd9rVrlxsE01K6NR2M6kCWZoZXeQzVInLwi"
 
-    auth = tweepy.OAuthHandler(_consumer_key, _consumer_secret)
-    auth.set_access_token(_access_token, _access_secret)
+    #auth = tweepy.OAuthHandler(_consumer_key, _consumer_secret)
+    #auth.set_access_token(_access_token, _access_secret)
+
+    oauth2_user_handler = tweepy.OAuth2UserHandler(
+    client_id = _client_id,
+    redirect_uri= _client_secret,
+    scope=["tweet.write"],
+    # Client Secret is only necessary if using a confidential client
+    client_secret=_client_secret
+)
+    print(oauth2_user_handler.url())
+
     api = tweepy.Client(consumer_key = _consumer_key, 
             consumer_secret = _consumer_secret,
             access_token = _access_token,
---
+++ telegramhandler.py
@@ -0,0 +1,39 @@
+from telegram.ext import Updater
+from telegram.update import Update
+
+creds = "5886078409:AAH5FFuGlYWsU0Rf_DTtDFCHt2rdMowbzAg"
+sponsorMessages = []
+
+#api key to tg
+updater = Updater(token=creds, use_context=True)
+job_queue = updater.job_queue
+
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Ready to take sponsor message")
+
+
+
+def append(update, context):
+    print(update.message.text.split(" ",1)[1])
+    sponsorMessages.append(update.message.text.split(" ",1)[1])
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sponsor message updated")
+
+
+#handles commands such as /start, /update
+from telegram.ext import CommandHandler, MessageHandler, Filters, PrefixHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+append_handler = PrefixHandler("/","add", append)
+dispatcher.add_handler(append_handler)
+
+def getSponsorMessage():
+    if sponsorMessages != []:
+        return sponsorMessages[len(sponsorMessages)-1]
+    else:
+        return ""
+
+updater.start_polling()
+
+
